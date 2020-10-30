80be5c081689cee61baab8207883ec6a1aef879e
Nur Askiah <askiahnur1@gmail.com>
2020-10-31 03:44:27 +0800

    update config

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 'http://https://library-dw18lsfsq.herokuapp.com/api/v1',
+  baseURL: 'https://library-dw18lsfsq.herokuapp.com/api/v1',
 });
 
 export const setToken = (token) => {
